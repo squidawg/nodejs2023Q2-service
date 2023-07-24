ca45742b84ba82c5bebb0c5def94d16e8cc2fba4
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-24 17:42:05 +0400

    fix: change error handling logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Res,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from '../dto/createUser.dto';
 import { UpdatePasswordDto } from '../dto/UpdatePasswordDto';
+import { ERROR_MSG, HTTP_CODE } from '../model/users.model';
+import { errorHandler } from '../utils/error-handler';
 
 @Controller('user')
 export class UsersController {
@@ -11,19 +22,35 @@ export class UsersController {
     return this.usersService.findAll();
   }
   @Get('/:id')
-  getUser(@Param('id') id: string) {
-    return this.usersService.findOne(id);
+  async getUser(@Param('id') id: string, @Res() response) {
+    const user = await this.usersService.findOne(id);
+    const err = errorHandler(user, ERROR_MSG.USER_ID);
+    return err
+      ? response.status(err.getStatus()).send(err.getResponse())
+      : response.status(HTTP_CODE.OK).send(user);
   }
   @Post()
   createUser(@Body() content: CreateUserDto) {
     return this.usersService.create(content);
   }
   @Put('/:id')
-  updateUser(@Param('id') id: string, @Body() content: UpdatePasswordDto) {
-    return this.usersService.update(id, content);
+  async updateUser(
+    @Param('id') id: string,
+    @Body() content: UpdatePasswordDto,
+    @Res() response,
+  ) {
+    const user = await this.usersService.update(id, content);
+    const err = errorHandler(user, ERROR_MSG.USER_ID);
+    return err
+      ? response.status(err.getStatus()).send(err.getResponse())
+      : response.status(HTTP_CODE.OK).send(user);
   }
   @Delete('/:id')
-  deleteUser(@Param('id') id: string) {
-    return this.usersService.delete(id);
+  async deleteUser(@Param('id') id: string, @Res() response) {
+    const user = await this.usersService.delete(id);
+    const err = errorHandler(user, ERROR_MSG.USER_ID);
+    return err
+      ? response.status(err.getStatus()).send(err.getResponse())
+      : response.status(HTTP_CODE.DELETED).send();
   }
 }
