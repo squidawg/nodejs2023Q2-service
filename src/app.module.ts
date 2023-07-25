98fb2f2be33904fb3d2cc604575bda344df55dbd
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-25 12:16:42 +0400

    feat: add artist route config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,16 +9,27 @@ import { TrackController } from './track/track.controller';
 import { TrackModule } from './track/track.module';
 import { TrackRepository } from './track/track.repository';
 import { TrackService } from './track/track.service';
+import { ArtistController } from './artist/artist.controller';
+import { ArtistModule } from './artist/artist.module';
+import { ArtistService } from './artist/artist.service';
+import { ArtistRepository } from './artist/artist.repository';
 
 @Module({
-  imports: [UsersModule, TrackModule],
-  controllers: [AppController, UsersController, TrackController],
+  imports: [UsersModule, TrackModule, ArtistModule],
+  controllers: [
+    AppController,
+    UsersController,
+    TrackController,
+    ArtistController,
+  ],
   providers: [
     AppService,
     UsersService,
     TrackService,
+    ArtistService,
     UsersRepository,
     TrackRepository,
+    ArtistRepository,
   ],
 })
 export class AppModule {}
