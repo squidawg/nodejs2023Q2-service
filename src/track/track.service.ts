5314b5457bbc2784135605b0e8e86c50f9cb58e3
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-30 18:31:55 +0400

    fix: change business logic for Track service

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,23 +1,60 @@
 import { Injectable } from '@nestjs/common';
-import { TrackRepository } from './track.repository';
 import { Track } from './model/track.model';
+import { v4, validate } from 'uuid';
+import { HTTP_CODE } from '../utils/util.model';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TrackEntity } from './entity/track.entity';
 
 @Injectable()
 export class TrackService {
-  constructor(private trackRepository: TrackRepository) {}
-  findOne(id: string) {
-    return this.trackRepository.findOne(id);
+  constructor(
+    @InjectRepository(TrackEntity) private repo: Repository<TrackEntity>,
+  ) {}
+  async findOne(id: string) {
+    if (!validate(id)) {
+      return HTTP_CODE.BAD_REQUEST;
+    }
+    const track = await this.repo.findOne({ where: { id } });
+    if (!track) {
+      return HTTP_CODE.NOT_FOUND;
+    }
+    return track;
   }
   findAll() {
-    return this.trackRepository.findAll();
+    return this.repo.find();
   }
-  create(content: Track) {
-    return this.trackRepository.create(content);
+  async create(content: Track) {
+    const id = v4();
+    const newTrack: Track = {
+      id,
+      ...content,
+    };
+    const track = await this.repo.create(newTrack);
+    await this.repo.save(track);
+    return newTrack;
   }
-  update(id: string, content: Track) {
-    return this.trackRepository.update(id, content);
+  async update(id: string, content: Track) {
+    if (!validate(id)) {
+      return HTTP_CODE.BAD_REQUEST;
+    }
+    const track = await this.repo.findOne({ where: { id } });
+    if (!track) {
+      return HTTP_CODE.NOT_FOUND;
+    }
+    const UpdatedTrack = { id, ...content };
+    await this.repo.save(UpdatedTrack);
+    return UpdatedTrack;
   }
-  delete(id: string) {
-    return this.trackRepository.delete(id);
+  async delete(id: string) {
+    if (!validate(id)) {
+      return HTTP_CODE.BAD_REQUEST;
+    }
+    const track = await this.repo.findOne({ where: { id } });
+    if (!track) {
+      return HTTP_CODE.NOT_FOUND;
+    }
+    Object.assign(track);
+    return this.repo.delete(track);
   }
 }
