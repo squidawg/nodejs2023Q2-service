ca0a09eb875accdd3a79b30e4eefcc41d484ada0
squidawg <78506807+squidawg@users.noreply.github.com>
2023-07-31 16:10:34 +0400

    fix: change business logic in favour to connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,55 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
-import { UsersRepository } from './users/users.repository';
-import { TrackController } from './track/track.controller';
 import { TrackModule } from './track/track.module';
-import { TrackRepository } from './track/track.repository';
-import { TrackService } from './track/track.service';
-import { ArtistController } from './artist/artist.controller';
 import { ArtistModule } from './artist/artist.module';
-import { ArtistService } from './artist/artist.service';
-import { ArtistRepository } from './artist/artist.repository';
 import { AlbumModule } from './album/album.module';
-import { AlbumController } from './album/album.controller';
-import { AlbumService } from './album/album.service';
-import { AlbumRepository } from './album/album.repository';
-import { FavoritesController } from './favorites/favorites.controller';
 import { FavoritesModule } from './favorites/favorites.module';
-import { FavoritesService } from './favorites/favorites.service';
-import { FavoritesRepository } from './favorites/favorites.repository';
+import { DatabaseModule } from './shared/typeorm.service';
+import { ConfigModule } from '@nestjs/config';
+import { getEnvPath } from './envs/envHelper';
+
+const envFilePath: string = getEnvPath(`dist/envs`);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
     UsersModule,
     TrackModule,
     ArtistModule,
     AlbumModule,
     FavoritesModule,
+    DatabaseModule,
   ],
-  controllers: [
-    AppController,
-    UsersController,
-    TrackController,
-    ArtistController,
-    AlbumController,
-    FavoritesController,
-  ],
-  providers: [
-    AppService,
-    UsersService,
-    TrackService,
-    ArtistService,
-    AlbumService,
-    FavoritesService,
-    UsersRepository,
-    TrackRepository,
-    ArtistRepository,
-    AlbumRepository,
-    FavoritesRepository,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
