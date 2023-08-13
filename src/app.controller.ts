8c9a576b03f9b493f2b5f2984ea95037c027a1c2
squidawg <78506807+squidawg@users.noreply.github.com>
2023-08-13 17:12:55 +0400

    feat: add decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Public } from './utils/decorator.service';
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
