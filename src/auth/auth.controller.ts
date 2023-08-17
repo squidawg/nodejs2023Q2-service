b498a84d96ea67c7aea9cf816669970b40273dc3
squidawg <78506807+squidawg@users.noreply.github.com>
2023-08-17 16:47:41 +0400

    feat: add swagger api tags

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,39 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { AuthService } from './auth.service';
-import { AuthDto } from './dto/auth.dto';
+import { AccessDto, AuthDto } from "./dto/auth.dto";
 import { RefreshDto } from './dto/refresh.dto';
-import { Public } from '../utils/decorator.service';
-@Public()
+import { ApiLoginPost, ApiPost, ApiRefreshAuth, Public } from "../utils/decorator.service";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { UserData } from "../users/model/users.model";
+@ApiTags('Auth Api')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+  @ApiPost(UserData)
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  @Public()
   @Post('/signup')
   async signup(@Body() content: AuthDto) {
     return await this.authService.signUp(content);
   }
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  @ApiLoginPost(AccessDto)
+  @Public()
   @Post('/login')
   async signin(@Body() content: AuthDto) {
     const user = await this.authService.validate(content);
     return await this.authService.login(user);
   }
+  @UsePipes(
+    new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNAUTHORIZED,
+    }),
+  )
+  @ApiRefreshAuth(AccessDto)
+  @ApiBearerAuth('access-token')
   @Post('/refresh')
   async refresh(@Body() content: RefreshDto) {
-    const token = this.authService.checkToken(content);
+    const token = await this.authService.checkToken(content);
     return await this.authService.login(token);
   }
 }
