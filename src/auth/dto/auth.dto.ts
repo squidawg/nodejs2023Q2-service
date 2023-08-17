88701db1e1e961bcd274b1d7b442bdf287cbff89
squidawg <78506807+squidawg@users.noreply.github.com>
2023-08-17 16:48:12 +0400

    feat: add access dto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,10 +1,24 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
 
 export class AuthDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'MusicListener' })
   login: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'mySecretPassword' })
   password: string;
+}
+
+export class AccessDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'string' })
+  accessToken: string;
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ example: 'string' })
+  refreshToken: string;
 }
