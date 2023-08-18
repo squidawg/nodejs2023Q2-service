8ec172d01172ee8481b91387425c7ac9c7b8bffc
squidawg <78506807+squidawg@users.noreply.github.com>
2023-08-18 14:01:45 +0400

    refactor: minor file structure changes

--- src/logger/logger.filehandler.ts
+++ src/logger/logger.filehandler.ts
@@ -2,7 +2,6 @@ import { appendFile, mkdir, readdir } from 'fs/promises';
 import * as fs from 'fs/promises';
 import { Injectable } from '@nestjs/common';
 import * as os from 'os';
-
 @Injectable()
 export class LogFileHandlerLogBase {
   logFilePath: string = '';
@@ -11,16 +10,16 @@ export class LogFileHandlerLogBase {
     if (this.logFilePath === '') {
       this.logFilePath =
         process.cwd() +
-        `/${process.env.PATH_LOGS}/${fileName}/${new Date().toISOString()}`;
+        `/${process.env.PATH_LOGS}/_${new Date().toISOString()}`;
     }
     try {
       await readdir(this.logFilePath);
     } catch (e) {
       await mkdir(this.logFilePath, { recursive: true });
     }
+
     const line = message + os.EOL;
     const source = `${this.logFilePath}/${fileName}.log`;
-
     await appendFile(source, line);
     const stat = await fs.stat(`${source}`);
     if (stat.size > this.MAX_FILE_SIZE_KB * 10) {
